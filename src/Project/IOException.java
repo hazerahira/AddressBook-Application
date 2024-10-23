608725927e0772bb855946bfde713d92502fa575
Hazera_Khatun_Hira <63002934+hazerahira@users.noreply.github.com>
2024-10-23 13:24:11 +0600

    Add files via upload

---
+++ src/Project/IOException.java
@@ -0,0 +1,19 @@
+package Project;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+class IOException {
+
+    void printStackTrace() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
+
---
+++ src/Project/People.java
@@ -0,0 +1,15 @@
+package Project;
+
+public class People {
+    public  String name ;
+    public  String phone_Number ;
+    public  String email ;
+    public  String address ;
+
+    public People(String name, String phone_Number, String email, String address) {
+        this.name = name;
+        this.phone_Number = phone_Number;
+        this.email = email;
+        this.address = address;
+    }
+}
